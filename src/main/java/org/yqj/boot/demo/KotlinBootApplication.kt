d0062ec651a5fbbc99d4f30b193c1fcba4bba01d
qijunyao <qijunyao@xiaohongshu.com>
2020-09-21 16:33:50 +0800

    add

--- src/main/java/org/yqj/boot/demo/KotlinBootApplication.kt
+++ src/main/java/org/yqj/boot/demo/KotlinBootApplication.kt
@@ -1,6 +1,5 @@
 package org.yqj.boot.demo
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 
